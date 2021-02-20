c551f20516df43784481206096d0bf0ef5d8da53
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-20 20:52:59 +0100

    3. Why we need to use JSX. Syntax of the JSX in writing React Code.

--- main.js
+++ main.js
@@ -5,4 +5,20 @@ const element = React.createElement(
 	React.createElement('h2', { className: 'leela' }, 'Leela Web Dev')
 )
 
-ReactDOM.render(element, document.getElementById('app'))
+const channelName = 'Leela Web Dev'
+
+function getChannelName() {
+	if (channelName === 'Leela Web Dev') return <div>{channelName}</div>
+	else return <div>Else Part</div>
+}
+
+const element2 = (
+	<div>
+		<div className='leela' tabIndex='1'>
+			Hello World with JSx
+		</div>
+		{getChannelName()}
+	</div>
+)
+
+ReactDOM.render(element2, document.getElementById('app'))
